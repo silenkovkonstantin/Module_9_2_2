a53333247fe606e0b043deba3e1a5862fdcd9912
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 09:51:32 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException("Сообщение об ошибке");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
